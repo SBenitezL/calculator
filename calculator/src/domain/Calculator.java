37a8a0de172bedc2ab30c5acdf0d517c5adf4dab
Santiago <sbenitez1607@gmail.com>
2024-10-18 12:49:55 -0500

    Add: implementation of functions and exception on division

--- calculator/src/domain/Calculator.java
+++ calculator/src/domain/Calculator.java
@@ -2,4 +2,27 @@ package domain;
 
 public class Calculator implements ICalculator {
 
+    @Override
+    public Double add(Double op1, Double op2) {
+        return op1 + op2;
+    }
+
+    @Override
+    public Double substraction(Double op1, Double op2) {
+        return op1 - op2;
+    }
+
+    @Override
+    public Double multiplication(Double op1, Double op2) {
+        return op1 * op2;
+    }
+
+    @Override
+    public Double division(Double op1, Double op2) throws Exception {
+        if (op2 == 0) {
+            throw new Exception("El operador 2 no puede ser 0");
+        }
+        return op1 / op2;
+    }
+
 }
